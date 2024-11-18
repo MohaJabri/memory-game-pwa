ac768b19edc0b5a8e690a5a4b0929c27a3bc45c1
Jabri <mohammed.jabri.developer@gmail.com>
2024-11-18 15:15:19 +0100

    HotFix V2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // src/app.js
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from './lib/lit.js';
 import './components/home-view.js';
 import './components/game-board.js';
 
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,28 @@ import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: 'src/app.js',
-      formats: ['es']
-    },
     rollupOptions: {
-      external: /^lit/
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      external: [/^lit/],
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+        globals: {
+          lit: 'lit'
+        }
+      }
     }
   },
   server: {
     port: 1234
   },
-  base: '/memory-game-pwa/'
+  base: '/memory-game-pwa/',
+  resolve: {
+    alias: {
+      'lit': resolve(__dirname, 'node_modules/lit/index.js')
+    }
+  }
 }); 
